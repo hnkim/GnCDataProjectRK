20de609e819dd424c83a31842f0074c66583bf5d
Ryan Kim <hnkim328@gmail.com>
2014-07-18 13:42:10 -0400

    GnC Data Course Project

---
+++ run_analysis.R
@@ -0,0 +1,30 @@
+getwd()
+setwd("~/Dropbox/Coursera/R codes/03_Getting_Cleaning Data")
+
+list.files("./proj_dataset/train")
+list.files("./proj_dataset/test")
+
+featureDF <- read.table("./proj_dataset/features.txt")
+head(featureDF)
+
+## Import Training Datasets
+trainXdf <- read.table("./proj_dataset/train/X_train.txt")
+trainYdf <- read.table("./proj_dataset/train/y_train.txt")
+
+## Import Test Datasets
+testXdf <- read.table("./proj_dataset/test/X_test.txt")
+testYdf <- read.table("./proj_dataset/test/y_test.txt")
+
+names(trainXdf) <- featureDF[,2]
+# names(trainXdf)
+
+names(testXdf) <- featureDF[,2]
+# names(testXdf)
+
+names(trainYdf) <- "labels"
+names(testYdf) <- "labels"
+
+trainDF <- cbind(trainXdf, trainYdf)
+testDF <- cbind(testXdf, testYdf)
+
+fullDF <- rbind(trainDF, testDF)
